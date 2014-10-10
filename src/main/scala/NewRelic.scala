bcce5239eec4d7d5bdfb4a48dc8721d06b60736a
Gary Coady <gcoady@gilt.com>
2014-10-10 16:57:13 +0100

    Update default New Relic version to 3.11.0
    Release notes:
    https://discuss.newrelic.com/t/java-agent-3-11-0-release-notes/8016

--- src/main/scala/NewRelic.scala
+++ src/main/scala/NewRelic.scala
@@ -26,7 +26,7 @@ object NewRelic extends AutoPlugin {
 
   def packagerSettings: Seq[Setting[_]] = Seq(
     ivyConfigurations += nrConfig,
-    newrelicVersion := "3.10.0",
+    newrelicVersion := "3.11.0",
     newrelicAgent := findNewrelicAgent(update.value),
     newrelicAppName := name.value,
     newrelicConfig := makeNewRelicConfig((target in Universal).value, newrelicConfigTemplate.value, newrelicTemplateReplacements.value),
